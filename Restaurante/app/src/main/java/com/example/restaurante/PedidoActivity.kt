cf50edb5337d850abca73971bb8c9ffe461880d3
Arthur Soares e Silva Pereira de Miranda <160657905+Arthursoares2008@users.noreply.github.com>
2024-05-09 17:35:13 -0300

    Update PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -11,17 +11,24 @@ class PedidoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+// Obtém os valores das quantidades de pizza, salada e hambúrguer da intent
         val i = intent
         val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
         val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
         val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
 
+// Calcula os preços com base nas quantidades
+        val precoPizza = quantidadePizza * 8
+        val precoSalada = quantidadeSalada * 10
+        val precoHamburguer = quantidadeHamburguer * 12
+
+// Cria uma string com o resumo do pedido
         val texto = "Resumo do Pedido\n" +
-                "Pizza: $quantidadePizza Preço: ${quantidadePizza*8}\n" +
-                "Salada: $quantidadeSalada Preço: ${quantidadeSalada*10}\n" +
-                "Hamburguer: $quantidadeHamburguer Preço: ${quantidadeHamburguer*12}\n"
+                "Pizza: $quantidadePizza Preço: $$precoPizza\n" +
+                "Salada: $quantidadeSalada Preço: $$precoSalada\n" +
+                "Hamburguer: $quantidadeHamburguer Preço: $$precoHamburguer\n"
 
+// Exibe o resumo na interface do usuário
         binding.textResumo.text = texto
-
     }
 }
