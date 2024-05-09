da83e133a184c1635612aa85e1dd6d9b66ea1dce
Arthur Soares e Silva Pereira de Miranda <160657905+Arthursoares2008@users.noreply.github.com>
2024-05-09 17:34:27 -0300

    Update MainActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
@@ -17,55 +17,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+// Obtém o nome de usuário da intent
         val username = intent.extras?.getString("username")
 
-        if(!username.isNullOrEmpty()){
+// Se o nome de usuário não estiver vazio, exibe uma saudação
+        if (!username.isNullOrEmpty()) {
             binding.textOla.setText("Olá $username")
         }
 
+// Configura o botão "Fechar" para finalizar a ação
         binding.buttonFechar.setOnClickListener {
             finishAffinity()
         }
 
+// Configura o botão "Pedir" para iniciar a SplashActivity com as quantidades de itens
         binding.buttonPedir.setOnClickListener {
             val i = Intent(this, SplashActivity::class.java)
-            i.putExtra("quantidadePizza", binding.editQuantidadePizza.text.toString())
-            i.putExtra("quantidadeSalada", binding.editQuantidadeSalada.text.toString())
-            i.putExtra("quantidadeHamburguer", binding.editQuantidadeHamburguer.text.toString())
+            i.putExtra("QuantidadePizza", binding.editQuantidadePizza.text.toString())
+            i.putExtra("QuantidadeSalada", binding.editQuantidadeSalada.text.toString())
+            i.putExtra("QuantidadeHamburguer", binding.editQuantidadeHamburguer.text.toString())
             startActivity(i)
             finish()
         }
 
-
+// Configura os checkboxes para atualizar as quantidades e visibilidade dos preços
+// Check box da pizza
         binding.checkPizza.setOnClickListener {
-            if(binding.checkPizza.isChecked()){
+            if (binding.checkPizza.isChecked()) {
                 binding.editQuantidadePizza.setText("1")
                 binding.textPrecoPizza.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.editQuantidadePizza.setText("0")
                 binding.textPrecoPizza.visibility = View.GONE
             }
         }
 
+// Check box da Salada
         binding.checkSalada.setOnClickListener {
-            if(binding.checkSalada.isChecked()){
+            if (binding.checkSalada.isChecked()) {
                 binding.editQuantidadeSalada.setText("1")
                 binding.textPrecoSalada.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.editQuantidadeSalada.setText("0")
                 binding.textPrecoSalada.visibility = View.GONE
             }
         }
 
+// Check box do Hamburguer
         binding.checkHamburger.setOnClickListener {
-            if(binding.checkHamburger.isChecked()){
+            if (binding.checkHamburger.isChecked()) {
                 binding.editQuantidadeHamburguer.setText("1")
                 binding.textPrecoHamburguer.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.editQuantidadeHamburguer.setText("0")
                 binding.textPrecoHamburguer.visibility = View.GONE
             }
         }
-
     }
 }
